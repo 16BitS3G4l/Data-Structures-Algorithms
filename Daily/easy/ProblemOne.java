62e11a9601f9cb944e5b6d82322e4ef8093179c9
dev-segal <ysegal7@student.touro.edu>
2021-01-18 15:33:08 -0500

    Added folder to store Daily Coding Problems

---
+++ Daily/easy/ProblemOne.java
@@ -0,0 +1,68 @@
+package easy;
+
+import java.util.HashMap;
+
+/* 
+    Problem Statement: 
+        Given a list of numbers and a number k, return whether any two
+        numbers from the list add up to k.
+        For example, given [10, 15, 3, 7] and k of 17, return true
+        since 10 + 7 is 17. 
+*/
+
+class ProblemOne {    
+  public static boolean doesSumExistBruteForce(int k, int[] arr) {
+    for(int i = 0; i < arr.length; i++) {
+        for(int j = 0; j < arr.length; j++) {
+            if(i == j) 
+                continue;
+            
+            if(arr[i] + arr[j] == k) {
+                return true;
+            }
+        }
+    }
+
+    return false;
+  }
+  
+  public static boolean doesSumExist(int k, int[] arr) {
+    HashMap<Integer, Boolean> map = new HashMap<>();
+
+    for(int element : arr) {
+        if( map.containsKey(element) ) {
+            return true;
+        } else {
+            map.put((k - element), true);
+        }
+    }
+
+    return false;
+  }
+
+    public static void main(String[] args) {
+        
+        // This is a test I used to showcase how much of a difference there exists between these two algorithms 
+
+        int[] arrOneTest = new int[60000];
+        
+        for(int i = 0; i < arrOneTest.length; i++) {
+            arrOneTest[i] = Math.round((long)Math.random()) * 1000;
+        }
+
+        System.out.println("Testing first way (linear): ");
+        System.out.println(doesSumExist(45, arrOneTest));
+
+        try {
+            Thread.sleep(1500);
+        } catch(Exception e) {
+            e.printStackTrace();
+        }
+
+        System.out.println("Testing second way (polynomial): ");
+        System.out.println(doesSumExistBruteForce(45, arrOneTest));
+
+
+    }
+
+}
